38377c95a501a8292a957fa28c9cd91fe7407178
F-O-P <tayler.coon@gmail.com>
2023-08-21 10:50:26 -0500

    Birb chases player
    Birb is now hungry enough to chase the player through the map.

--- enemy/enemy_birb.gd
+++ enemy/enemy_birb.gd
@@ -1,39 +1,17 @@
 extends CharacterBody2D
 
-@export var movement_speed: float = 200.0
+const speed = 135.0
+@export var player: Node2D
+@onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
 
-@export var movement_target: Node2D
-@export var navigation_agent: NavigationAgent2D
-
-
-func _ready():
-	navigation_agent.path_desired_distance = 4.0
-	navigation_agent.target_desired_distance = 4.0
-	
-	call_deferred("actor_setup")
-
-
-
-func actor_setup():
-	await get_tree().physics_frame
-	
-	set_movement_target(movement_target.position)
-
-
-func set_movement_target(target_point: Vector2):
-	navigation_agent.target_position = target_point
-
-
-func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
-		return
-	
-	var current_agent_position: Vector2 = global_position
-	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-	
-	var new_velocity: Vector2 = next_path_position - current_agent_position
-	new_velocity = new_velocity.normalized()
-	new_velocity = new_velocity * movement_speed
-	
-	velocity = new_velocity
+func _physics_process(delta: float) -> void:
+	var dir = to_local(navigation_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
 	move_and_slide()
+
+func makepath() -> void:
+	navigation_agent.target_position = player.global_position
+	
+
+func _on_timer_timeout():
+	makepath()
